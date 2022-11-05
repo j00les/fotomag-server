8371a7c59f721c9bd81f813a5df40c68d52a6ad4
Alexchandrahanif <alexchandra1998@gmail.com>
2022-11-06 02:15:14 +0700

    selesai transaction

--- services/routes/transaction.js
+++ services/routes/transaction.js
@@ -4,5 +4,9 @@ const transactionRouter = require("express").Router();
 
 transactionRouter.post("/:idAtk", Controller.createTransaction);
 transactionRouter.patch("/:id", Controller.changeStatus);
+transactionRouter.get("/history/:id", Controller.history);
+transactionRouter.get("/listTransaction/:id", Controller.listTransaction);
+transactionRouter.get("/courier/:id", Controller.listCourier);
+transactionRouter.get("/customer/:id", Controller.listCustomer);
 
 module.exports = transactionRouter;
