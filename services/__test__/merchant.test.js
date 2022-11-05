8bf69b40be489b522692b58ef440cc1ff102c93d
Farfour07 <dimassds07@gmail.com>
2022-11-06 02:24:02 +0700

    update testing 1

--- services/__test__/merchant.test.js
+++ services/__test__/merchant.test.js
@@ -1,4 +1,4 @@
-const { sequelize, User } = require("../models");
+const { sequelize, User, ATK } = require("../models");
 const { queryInterface } = sequelize;
 const request = require("supertest");
 const app = require("../app");
@@ -8,24 +8,34 @@ beforeAll(async () => {
     "Users",
     [
       {
-        name: "ucok1",
-        email: "ucok1@mail.com",
-        password: "asd123",
-        // balance: 0,
-        address:
-          "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
-        role: "Customer",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
         name: "baba1",
         email: "baba1@mail.com",
         // balance: 0,
         password: "asd123",
         address:
           "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
-        role: "Seller",
+        role: "Merchant",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ],
+    null
+  );
+  await queryInterface.bulkInsert(
+    "ATKs",
+    [
+      {
+        name: "Printing Doa Ibu",
+        address:
+          "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
+        priceColor: 2000,
+        priceBlack: 1000,
+        priceJilid: 10000,
+        location: sequelize.fn(
+          "ST_GeomFromText",
+          "POINT(37.4220936 -122.083922)"
+        ),
+        UserId: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -40,61 +50,64 @@ afterAll(async () => {
     restartIdentity: true,
     cascade: true,
   });
+  await queryInterface.bulkDelete("ATKs", null, {
+    truncate: true,
+    restartIdentity: true,
+    cascade: true,
+  });
 });
 
-describe("Hitting endpoint /register", () => {
-  test("POST new customer with correct input", () => {
+describe("Register new merchant", () => {
+  test("POST new merchant with correct input", () => {
     return request(app)
-      .post("/customer/registers")
+      .post("/servicer/register")
       .send({
-        name: "ucok2",
-        email: "ucok2@mail.com",
+        name: "baba2",
+        email: "baba2@mail.com",
         balance: 0,
         password: "asd123",
         address:
           "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
-        role: "Customer",
+        role: "Merchant",
+        atkName: "Printing Doa Ibu",
+        atkAddress:
+          "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
+        priceColor: 1000,
+        priceBlack: 100,
+        priceJilid: 5000,
       })
       .then((response) => {
         expect(response.statusCode).toBe(201);
         expect(response.body).toHaveProperty("id", expect.any(Number));
-        // expect(response.body).toHaveProperty("role", "Customer");
+        expect(response.body).toHaveProperty("email", expect.any(String));
+        // expect(response.body).toHaveProperty("role", "Seller");
       });
   });
-  // test("POST new seller with correct input", () => {
-  //   return request(app)
-  //     .post("/registers")
-  //     .send({
-  //       name: "baba2",
-  //       email: "baba2@mail.com",
-  //       // balance: 0,
-  //       password: "asd123",
-  //       address:
-  //         "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
-  //       role: "Seller",
-  //     })
-  //     .then((response) => {
-  //       expect(response.statusCode).toBe(201);
-  //       expect(response.body).toHaveProperty("id", expect.any(Number));
-  //       expect(response.body).toHaveProperty("role", "Seller");
-  //     });
-  // });
-  // test("POST new user without email", () => {
-  //   return request(app)
-  //     .post("/users")
-  //     .send({
-  //       name: "baba2",
-  //       balance: 0,
-  //       password: "asd123",
-  //       address:
-  //         "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
-  //       role: "Seller",
-  //     })
-  //     .then((response) => {
-  //       expect(response.statusCode).toBe(400);
-  //       expect(response.body).toHaveProperty("msg", "Email is required");
-  //     });
-  // });
+
+  test("POST new merchant without email", () => {
+    return request(app)
+      .post("/servicer/register")
+      .send({
+        name: "baba2",
+        balance: 0,
+        password: "asd123",
+        address:
+          "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
+        role: "Merchant",
+        atkName: "Printing Doa Ibu",
+        atkAddress:
+          "Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby. Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240",
+        priceColor: 1000,
+        priceBlack: 100,
+        priceJilid: 5000,
+      })
+      .then((response) => {
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toBeInstanceOf(Object);
+        expect(response.body.message).toEqual(["Email is required"]);
+        // expect(response.body).toHaveProperty("role", "Seller");
+      });
+  });
   // test("POST new user with existing email", () => {
   //   return request(app)
   //     .post("/users")
