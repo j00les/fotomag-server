20ec7caf2d9661b0e71821bc322030c3ddfc7327
Alexchandrahanif <alexchandra1998@gmail.com>
2022-11-08 16:59:21 +0700

    deploy

--- services/app.js
+++ services/app.js
@@ -7,7 +7,7 @@ const handleErrors = require("./middleware/handleError");
 const router = require("./routes");
 const dotenv = require("dotenv");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // const cloudinary = require('cloudinary').v2
 const cloudinary = require("./config/cloudinaryConfig");
 
